9a4ee93ded67ae7b987875e44c134866101b6eed
Joonhak <ofnullable@gmail.com>
2019-07-02 18:42:20 +0900

    Add new project using next-create-app

---
+++ front-deprecated/pages/index.js
@@ -0,0 +1,118 @@
+import React, { useEffect } from 'react';
+import { Col, Row, PageHeader } from 'antd';
+import { useSelector, useDispatch } from 'react-redux';
+
+import Balance from '../components/Balance';
+import OrderBook from '../components/OrderBook';
+import History from '../components/History';
+import TokenBar from '../components/TokenBar';
+import TradeForm from '../components/TradeForm';
+import { generateJsonRpcId } from '../utils/jsonrpc';
+import {
+  getBuyOrderListEvent,
+  getSellOrderListEvent,
+  getAddressEvent,
+} from '../utils/events';
+import actionTypes from '../redux/actionTypes';
+
+const Home = () => {
+  const { address } = useSelector(state => state.iconex);
+  const { selectedToken } = useSelector(state => state.tokens);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const eventHandler = e => {
+      const { type, payload } = e.detail;
+      dispatch({
+        type,
+        payload,
+      });
+    };
+
+    window.addEventListener(actionTypes.ICONEX_RELAY_RESPONSE, eventHandler);
+    return () => {
+      window.removeEventListener(
+        actionTypes.ICONEX_RELAY_RESPONSE,
+        eventHandler
+      );
+    };
+  }, []);
+
+  useEffect(() => {
+    window.onload = () => {
+      if (!address) {
+        window.dispatchEvent(getAddressEvent());
+      }
+
+      const ids = _requestOrderList();
+      dispatch({
+        type: actionTypes.LOAD_BUY_ORDER_REQUEST,
+        id: ids[0],
+        token: selectedToken.address,
+      });
+      dispatch({
+        type: actionTypes.LOAD_SELL_ORDER_REQUEST,
+        id: ids[1],
+        token: selectedToken.address,
+      });
+    };
+  }, [selectedToken]);
+
+  const _requestOrderList = () => {
+    const buyId = generateJsonRpcId();
+    const buyEvent = getBuyOrderListEvent(buyId);
+
+    const sellId = generateJsonRpcId();
+    const sellEvent = getSellOrderListEvent(sellId);
+
+    window.dispatchEvent(buyEvent);
+    window.dispatchEvent(sellEvent);
+
+    return [buyId, sellId];
+  };
+
+  return (
+    <div>
+      <Row gutter={8} style={{ margin: 0 }}>
+        <Col xs={0} md={0} lg={1} />
+        <Col xs={24} md={6} lg={6} style={{ marginTop: '10px' }}>
+          <Row>
+            <Col>
+              <Balance />
+            </Col>
+            <Col style={{ marginTop: '10px' }}>
+              <TradeForm />
+            </Col>
+          </Row>
+        </Col>
+        <Col xs={24} md={12} lg={10} style={{ marginTop: '10px' }}>
+          <PageHeader
+            title={`Order Book - ${selectedToken.symbol}`}
+            subTitle={`${selectedToken.symbol}/ICX - ${
+              selectedToken.currentPrice
+            }`}
+          />
+          <Row gutter={8} style={{ margin: 0 }}>
+            <Col xs={24} md={24} lg={24}>
+              <OrderBook />
+            </Col>
+            <Col xs={24} style={{ marginTop: '10px' }} />
+            <Col xs={24} style={{ marginTop: '10px' }}>
+              <History />
+            </Col>
+          </Row>
+        </Col>
+        <Col xs={24} md={6} lg={6} style={{ marginTop: '10px' }}>
+          <Row gutter={8} style={{ margin: 0 }}>
+            <Col xs={24}>
+              <TokenBar />
+            </Col>
+          </Row>
+        </Col>
+        <Col xs={0} md={0} lg={1} />
+      </Row>
+    </div>
+  );
+};
+
+export default Home;
--- front/pages/index.js
+++ front/pages/index.js
@@ -1,118 +1,91 @@
-import React, { useEffect } from 'react';
-import { Col, Row, PageHeader } from 'antd';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react'
+import Link from 'next/link'
+import Head from '../components/head'
+import Nav from '../components/nav'
 
-import Balance from '../components/Balance';
-import OrderBook from '../components/OrderBook';
-import History from '../components/History';
-import TokenBar from '../components/TokenBar';
-import TradeForm from '../components/TradeForm';
-import { generateJsonRpcId } from '../utils/jsonrpc';
-import {
-  getBuyOrderListEvent,
-  getSellOrderListEvent,
-  getAddressEvent,
-} from '../utils/events';
-import actionTypes from '../redux/actionTypes';
+const Home = () => (
+  <div>
+    <Head title="Home" />
+    <Nav />
 
-const Home = () => {
-  const { address } = useSelector(state => state.iconex);
-  const { selectedToken } = useSelector(state => state.tokens);
-  const dispatch = useDispatch();
+    <div className="hero">
+      <h1 className="title">Welcome to Next!</h1>
+      <p className="description">
+        To get started, edit <code>pages/index.js</code> and save to reload.
+      </p>
 
-  useEffect(() => {
-    const eventHandler = e => {
-      const { type, payload } = e.detail;
-      dispatch({
-        type,
-        payload,
-      });
-    };
-
-    window.addEventListener(actionTypes.ICONEX_RELAY_RESPONSE, eventHandler);
-    return () => {
-      window.removeEventListener(
-        actionTypes.ICONEX_RELAY_RESPONSE,
-        eventHandler
-      );
-    };
-  }, []);
-
-  useEffect(() => {
-    window.onload = () => {
-      if (!address) {
-        window.dispatchEvent(getAddressEvent());
-      }
-
-      const ids = _requestOrderList();
-      dispatch({
-        type: actionTypes.LOAD_BUY_ORDER_REQUEST,
-        id: ids[0],
-        token: selectedToken.address,
-      });
-      dispatch({
-        type: actionTypes.LOAD_SELL_ORDER_REQUEST,
-        id: ids[1],
-        token: selectedToken.address,
-      });
-    };
-  }, [selectedToken]);
-
-  const _requestOrderList = () => {
-    const buyId = generateJsonRpcId();
-    const buyEvent = getBuyOrderListEvent(buyId);
-
-    const sellId = generateJsonRpcId();
-    const sellEvent = getSellOrderListEvent(sellId);
-
-    window.dispatchEvent(buyEvent);
-    window.dispatchEvent(sellEvent);
-
-    return [buyId, sellId];
-  };
-
-  return (
-    <div>
-      <Row gutter={8} style={{ margin: 0 }}>
-        <Col xs={0} md={0} lg={1} />
-        <Col xs={24} md={6} lg={6} style={{ marginTop: '10px' }}>
-          <Row>
-            <Col>
-              <Balance />
-            </Col>
-            <Col style={{ marginTop: '10px' }}>
-              <TradeForm />
-            </Col>
-          </Row>
-        </Col>
-        <Col xs={24} md={12} lg={10} style={{ marginTop: '10px' }}>
-          <PageHeader
-            title={`Order Book - ${selectedToken.symbol}`}
-            subTitle={`${selectedToken.symbol}/ICX - ${
-              selectedToken.currentPrice
-            }`}
-          />
-          <Row gutter={8} style={{ margin: 0 }}>
-            <Col xs={24} md={24} lg={24}>
-              <OrderBook />
-            </Col>
-            <Col xs={24} style={{ marginTop: '10px' }} />
-            <Col xs={24} style={{ marginTop: '10px' }}>
-              <History />
-            </Col>
-          </Row>
-        </Col>
-        <Col xs={24} md={6} lg={6} style={{ marginTop: '10px' }}>
-          <Row gutter={8} style={{ margin: 0 }}>
-            <Col xs={24}>
-              <TokenBar />
-            </Col>
-          </Row>
-        </Col>
-        <Col xs={0} md={0} lg={1} />
-      </Row>
+      <div className="row">
+        <Link href="https://github.com/zeit/next.js#getting-started">
+          <a className="card">
+            <h3>Getting Started &rarr;</h3>
+            <p>Learn more about Next on Github and in their examples</p>
+          </a>
+        </Link>
+        <Link href="https://open.segment.com/create-next-app">
+          <a className="card">
+            <h3>Examples &rarr;</h3>
+            <p>
+              Find other example boilerplates on the{' '}
+              <code>create-next-app</code> site
+            </p>
+          </a>
+        </Link>
+        <Link href="https://github.com/segmentio/create-next-app">
+          <a className="card">
+            <h3>Create Next App &rarr;</h3>
+            <p>Was this tool helpful? Let us know how we can improve it</p>
+          </a>
+        </Link>
+      </div>
     </div>
-  );
-};
 
-export default Home;
+    <style jsx>{`
+      .hero {
+        width: 100%;
+        color: #333;
+      }
+      .title {
+        margin: 0;
+        width: 100%;
+        padding-top: 80px;
+        line-height: 1.15;
+        font-size: 48px;
+      }
+      .title,
+      .description {
+        text-align: center;
+      }
+      .row {
+        max-width: 880px;
+        margin: 80px auto 40px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+      }
+      .card {
+        padding: 18px 18px 24px;
+        width: 220px;
+        text-align: left;
+        text-decoration: none;
+        color: #434343;
+        border: 1px solid #9b9b9b;
+      }
+      .card:hover {
+        border-color: #067df7;
+      }
+      .card h3 {
+        margin: 0;
+        color: #067df7;
+        font-size: 18px;
+      }
+      .card p {
+        margin: 0;
+        padding: 12px 0 0;
+        font-size: 13px;
+        color: #333;
+      }
+    `}</style>
+  </div>
+)
+
+export default Home
